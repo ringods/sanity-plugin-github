d166f24b480c8a398782cd117d06bebab4e051d6
Ringo De Smet <ringo@de-smet.name>
2022-01-19 21:53:42 +0100

    Bootstrapping the Gist content type with input component
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

---
+++ src/schema/gist.ts
@@ -0,0 +1,36 @@
+import GistInput from "../components/GistInput";
+
+export type Gist = {
+    _type: string;
+    _key?: string;
+    _version: number;
+    id: string;
+    file: string;
+};
+
+export default {
+    name: 'github.gist',
+    description: 'Gist',
+    type: 'object',
+    fields: [
+        {
+            type: 'string',
+            name: 'id',
+        },
+        {
+            type: 'string',
+            name: 'file',
+        },
+    ],
+    inputComponent: GistInput, // - https://github.com/sanity-io/sanity/blob/next/packages/%40sanity/code-input/src/schema.tsx#L15
+    preview: { //- https://github.com/sanity-io/sanity/blob/next/packages/%40sanity/code-input/src/schema.tsx#L45-L64
+        select: {
+            id: 'id',
+            file: 'file'
+        }
+    }
+};
+
+// docs
+// https://www.sanity.io/docs/previews-list-views
+// https://www.sanity.io/docs/object-type definition
