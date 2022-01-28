ef38cb5c91ef55578fe9abf5eeddb59a7ed8fdc7
Ringo De Smet <ringo@de-smet.name>
2022-01-28 11:30:04 +0100

    Use the FormBuilder API to edit the Gist type
    Based on [`Working with objects and FormBuilderInput`](https://www.sanity.io/docs/custom-input-widgets#40b31a232a03)
    
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

--- src/components/GistInput.tsx
+++ src/components/GistInput.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { FormField } from '@sanity/base/components';
-import { Label, Text, TextInput } from '@sanity/ui';
-import PatchEvent, { set, unset } from '@sanity/form-builder/PatchEvent'
-
-import { useId } from "@reach/auto-id" // hook to generate unique IDs
+import { FormFieldPresence } from '@sanity/base/presence'
+import { FormBuilderInput } from '@sanity/form-builder/lib/FormBuilderInput'
+import Fieldset from 'part:@sanity/components/fieldsets/default'
+import { setIfMissing } from '@sanity/form-builder/PatchEvent'
+import { Marker, Path, SchemaType } from '@sanity/types'
 
 export interface GistValue {
   _type?: 'github.gist'
@@ -15,14 +15,22 @@ export interface GistType {
   name?: string
   title?: string
   description?: string
-  fields: {name: string; title?: string; placeholder?: string}[]
+  fields: {name: string; title?: string; placeholder?: string, type: SchemaType }[]
   options?: {
     hotspot?: boolean
   }
 }
 export interface GistInputProps {
   type: GistType,
-  value: GistValue,
+  value?: GistValue,
+  compareValue?: GistValue,
+  focusPath: Path,
+  markers: Marker[],
+  presence: FormFieldPresence[],
+  onBlur: () => void,
+  onChange: (...args: any[]) => void,
+  onFocus: (path: Path) => void,
+  level: number
 }
 
 const GistInput = React.forwardRef(
@@ -33,17 +41,79 @@ const GistInput = React.forwardRef(
     const {
       type,         // Schema information
       value,        // Current field value
+      compareValue, // Value to check for "edited" functionality
+      focusPath,    // focus Path
+    //   readOnly,     // Boolean if field is not editable
+    //   placeholder,  // Placeholder text from the schema
+      markers,      // Markers including validation rules
+      presence,     // Presence information for collaborative avatars
+      onFocus,      // Method to handle focus state
+      onBlur,       // Method to handle blur state
+      onChange,      // Method to handle patch events
+      level
     } = props
 
+    const handleFieldChange = React.useCallback(
+      (field, fieldPatchEvent) => {
+        // fieldPatchEvent is an array of patches
+        // Patches look like this:
+        /*
+            {
+                type: "set|unset|setIfMissing",
+                path: ["fieldName"], // An array of fields
+                value: "Some value" // a value to change to
+            }
+        */
+        onChange(fieldPatchEvent.prefixAll(field.name).prepend(setIfMissing({ _type: type.name })))
+      },
+      [onChange]
+    )
+
+    // Get an array of field names for use in a few instances in the code
+    const fieldNames = type.fields.map((f) => f.name)
+
+    // If Presence exist, get the presence as an array for the children of this field
+    const childPresence =
+      presence.length === 0
+        ? presence
+        : presence.filter((item) => fieldNames.includes(item.path[0]))
+
+    // If Markers exist, get the markers as an array for the children of this field
+    const childMarkers =
+      markers.length === 0 
+        ? markers 
+        : markers.filter((item) => fieldNames.includes(item.path[0]))
+
     return (
-      <FormField
-        description={type.description}  // Creates description from schema
-        title={type.title}              // Creates label from schema title
+      <Fieldset
+        legend={type.title} // schema title
+        description={type.description} // schema description
+        markers={childMarkers} // markers built above
+        presence={childPresence} // presence built above
       >
-        <TextInput
-          value={value.id}              // Current field value
-        />
-      </FormField>
+        {type.fields.map((field, i) => {
+          return (
+            // Delegate to the generic FormBuilderInput. It will resolve and insert the actual input component
+            // for the given field type
+            <FormBuilderInput
+              level={level + 1}
+              ref={i === 0 ? ref : null}
+              key={field.name}
+              type={field.type}
+              value={value && value[field.name]}
+              onChange={(patchEvent) => handleFieldChange(field, patchEvent)}
+              path={[field.name]}
+              markers={markers}
+              focusPath={focusPath}
+              readOnly={field.type.readOnly}
+              presence={presence}
+              onFocus={onFocus}
+              onBlur={onBlur}
+              compareValue={compareValue}
+            />
+          )
+        })}
+      </Fieldset>
     )
   }
 )
